788dcffc762062f53a237611f69a61c232cd5413
tiagom-dev <tiagom_dev@proton.me>
2022-12-23 15:51:12 +0000

    Add day 1 part 1 and 2 and add git ignore file

---
+++ src/main/java/org/example/Day1.java
@@ -0,0 +1,45 @@
+package org.example;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Day1 {
+    private static final List<Integer> results = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+
+        final File file = new File("src/main/resources/day1.txt");
+        final List<String> data = Files.readAllLines(file.toPath());
+        System.out.println(part1(data));
+        System.out.println(part2());
+    }
+
+    private static int part1(List<String> lines) {
+        int current = 0;
+
+        for (String line : lines) {
+            if (line.trim().isEmpty()) {
+                results.add(current);
+                current = 0;
+            } else {
+                current += Integer.parseInt(line.trim());
+            }
+        }
+        Collections.sort(results);
+        return Collections.max(results);
+    }
+
+    private static int part2() {
+        int result = 0;
+
+        for (int i = 0; i < 3; i++) {
+            result += Collections.max(results);
+            results.remove(Collections.max(results));
+        }
+        return result;
+    }
+}
